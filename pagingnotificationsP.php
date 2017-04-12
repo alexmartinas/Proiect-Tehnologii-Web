a5a9facbe82bcb17ec8e5becf11b2c5e0ec8e9ec
Dalv12 <scurtuvlad@gmail.com>
2017-04-12 22:56:06 +0300

    Add files via upload

--- pagingnotificationsP.php
+++ pagingnotificationsP.php
@@ -1,6 +1,5 @@
 <?php
 	
-	$id_tutore=0;
 
 	//Oracle DB user name
 	$contOracle = 'vlad';
@@ -17,6 +16,20 @@
 	$parolaOracle,
 	$connection_string
 	);
+
+	session_start();
+	$user_session=$_SESSION['username'];
+	$stid = oci_parse($connection, 'select id_user from users where username like :v_user_session');
+	oci_bind_by_name($stid, ":v_user_session", $user_session);
+	oci_execute($stid);
+	while ($row = oci_fetch_array ($stid,OCI_NUM)) {
+    foreach($row as $data) 
+    	{
+    	   $trololo = 123; 
+    	 }
+	}
+
+	$id_interest = $data;
 
 
 	$page = $_GET["page"];
@@ -35,9 +48,10 @@
 	$pagevfin=$pagev+20;
 
 	$nrrows = 0;
-	$stidl = oci_parse($connection, 'select id_child,date_n,id_interest,distance from (select id_child,date_n,id_interest,distance,ROWNUM r from notifications) where r between :v_page1 and :v_page2');
+	$stidl = oci_parse($connection, 'select id_child,date_n,id_interest,distance from (select id_child,date_n,id_interest,distance,ROWNUM r from notifications) where r between :v_page1 and :v_page2 and id_interest=:v_id_interest');
 	oci_bind_by_name($stidl, ":v_page1", $pagev);
 	oci_bind_by_name($stidl, ":v_page2", $pagevfin);
+	oci_bind_by_name($stidl, ":v_id_interest", $id_interest);
 	oci_execute($stidl);
 
 
@@ -51,7 +65,8 @@
 
 	}
 
-	$stid = oci_parse($connection, 'select * from notifications');
+	$stid = oci_parse($connection, 'select * from notifications where id_interest=:v_id_interest');
+	oci_bind_by_name($stid, ":v_id_interest", $id_interest);
 	oci_execute($stid);
 
 	while ($row = oci_fetch_array ($stid,OCI_NUM)) {
