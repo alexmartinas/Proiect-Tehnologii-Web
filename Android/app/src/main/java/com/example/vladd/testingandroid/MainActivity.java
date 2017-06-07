a08928a9f2085db38f1f3f2502ed47df269a3247
Dalv12 <scurtuvlad@gmail.com>
2017-06-07 16:29:57 +0300

    added accelerometer values on phone

--- Android/app/src/main/java/com/example/vladd/testingandroid/MainActivity.java
+++ Android/app/src/main/java/com/example/vladd/testingandroid/MainActivity.java
@@ -18,13 +18,29 @@ import com.android.volley.toolbox.Volley;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private TextView xText, yText, zText;
+    private Sensor mySensor;
+    private SensorManager SM;
+    private int count;
+
 
     EditText edtTen,edtMail;
     Button btnGoi;
     String URL_POST = "http://192.168.2.102/demoandroid/post.php";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edtTen = (EditText) findViewById(R.id.editText);
@@ -40,6 +56,48 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        //Create Sensor Manager
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        //Accelerometer Sensor
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //Register sensor Listener
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //Assign TextView
+        xText = (TextView)findViewById(R.id.xText);
+        yText = (TextView)findViewById(R.id.yText);
+        zText = (TextView)findViewById(R.id.zText);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        xText.setText("X: " + event.values[0]);
+        yText.setText("Y: " + event.values[1]);
+        zText.setText("Z: " + event.values[2]);
+
+        if(event.values[0] > 25) {
+            System.out.println("PROBLEMA LA X" + event.values[0]); count++;
+        }
+        if(event.values[1] > 25) {
+            System.out.println("PROBLEMA LA Y" + event.values[1]); count++;
+        }
+        if(event.values[2] > 25) {
+            System.out.println("PROBLEMA LA Z" + event.values[2]); count++;
+        }
+        if(count>5) {
+            System.out.println("SIGNAL ACCIDENT"); count=0;
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        //Not in use
     }
 
     private void InsertSV() {
